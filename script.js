7a319dfde415b1267adf1fb6c7ae6f572c8179a5
devika kumari <devika21@navgurukul.org>
2023-06-26 15:59:58 +0530

    first Commit

---
+++ script.js
@@ -0,0 +1,17 @@
+const box= document.getElementById("row");
+const button1 = document.getElementById("button1");
+button1.onclick=function(){
+    box.style.backgroundColor="Blue";
+};
+
+const button2 = document.getElementById("button2");
+button2.onclick=function(){
+    box.style.backgroundColor="green";
+};
+
+const button3 = document.getElementById("button3");
+button3.onclick=function(){
+    box.style.backgroundColor="yellow";
+};
+
+
